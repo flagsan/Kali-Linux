263b039633ec00f297fe3b222535aa58b84b2037
flag3 <123287726+flagsan@users.noreply.github.com>
2025-02-10 10:28:30 +0900

    add basic tools

--- setting.sh
+++ setting.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+# Create Tools directory
+mkdir -p ~/Tools/Linux
+mkdir -p ~/Tools/Windows
+mkdir -p ~/Tools/Common
+
+sudo apt-get update
+
+sudo apt-get install -y \
+  seclists \
+  enum4linux-ng \
+
+sudo apt-get clean
+# --- Linux --- #
+# Install pspy binaries
+wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy32 -O ~/Tools/Linux/pspy32
+wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64 -O ~/Tools/Linux/pspy64
+
+# --- Windows --- #
+# PSTools
+wget https://download.sysinternals.com/files/PSTools.zip -O ~/Tools/Windows/PSTools.zip
+unzip ~/Tools/Windows/PSTools.zip -d ~/Tools/Windows/PSTools
