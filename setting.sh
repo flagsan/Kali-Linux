6ed160a4409c31c2b596222f06378364308d7fc1
flag3 <123287726+flagsan@users.noreply.github.com>
2025-02-11 11:10:41 +0900

    add windows chisel

--- setting.sh
+++ setting.sh
@@ -21,6 +21,10 @@ wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64 -O ~
 
 # --- Windows --- #
 # PSTools
-wget https://download.sysinternals.com/files/PSTools.zip -O ~/Tools/Windows/PSTools.zip
+wget https://download.sysinternals.com/files/PSTools.zip -O ~/Tools/Windows/PSTools.zip -q
 unzip ~/Tools/Windows/PSTools.zip -d ~/Tools/Windows/PSTools
 rm ~/Tools/Windows/PSTools.zip
+
+wget https://github.com/jpillora/chisel/releases/download/v1.10.1/chisel_1.10.1_windows_amd64.gz -O ~/Tools/Windows/chisel.gz -q
+gunzip ~/Tools/Windows/chisel.gz
+mv ~/Tools/Windows/chisel ~/Tools/Windows/chisel.exe
