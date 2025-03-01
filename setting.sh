4d17424b91e45355d3e62bdaf9606c651e163d02
flag3 <123287726+flagsan@users.noreply.github.com>
2025-03-01 22:51:04 +0900

    add rustscan

--- setting.sh
+++ setting.sh
@@ -18,7 +18,16 @@ sudo apt install -y \
 
 sudo apt clean
 
-# --- Linux --- #
+# --- Commona --- #
+# Rustscan
+wget https://github.com/RustScan/RustScan/releases/download/2.4.1/rustscan.deb.zip
+unzip rustscan.deb.zip
+rm rustscan.deb.zip
+sudo dpkg -i rustscan_2.4.1-1_amd64.deb
+sudo apt install -f
+rm rustscan_2.4.1-1_amd64.deb
+
+# --- Linux Tools --- #
 # pspy
 wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy32 -O ~/Tools/Linux/pspy32 -q
 wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64 -O ~/Tools/Linux/pspy64 -q
@@ -28,7 +37,7 @@ wget https://github.com/jpillora/chisel/releases/download/v1.10.1/chisel_1.10.1_
 gunzip ~/Tools/Linux/chisel.gz
 chmod +x ~/Tools/Linux/chisel
 
-# --- Windows --- #
+# --- Windows Tools --- #
 # PSTools
 wget https://download.sysinternals.com/files/PSTools.zip -O ~/Tools/Windows/PSTools.zip -q
 unzip ~/Tools/Windows/PSTools.zip -d ~/Tools/Windows/PSTools
